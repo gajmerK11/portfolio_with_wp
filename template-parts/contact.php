d0c2e540ab29dbd159777948100097976492cbd0
kumar <gajmerk17@gmail.com>
2026-07-24 16:39:54 +0545

    styling fixes

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -28,7 +28,7 @@ $cp_phone = apply_filters( 'portfolio_contact_phone', '' );
 	<!-- Back -->
 	<button type="button" class="cp-back" data-contact-close>
 		<span class="cp-back-ico" aria-hidden="true">
-			<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H5M12 19l-7-7 7-7"></path></svg>
+			<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><path d="M24 12H8M15 19l-7-7 7-7"></path></svg>
 		</span>
 		<span class="cp-back-label"><?php esc_html_e( 'Back', 'portfolio' ); ?></span>
 	</button>
